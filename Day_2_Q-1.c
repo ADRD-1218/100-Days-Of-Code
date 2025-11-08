f07a17e22c7b838f8c75816013c964cf17b42d77
ADRD-1218 <aadirakshit1218@gmail.com>
2025-11-08 08:37:37 +0000

    Day 2

---
+++ Day_2_Q-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, prod;
+    printf("Enter Length = \n");
+    scanf("%d", &a);
+    printf("Enter Breadth = \n");
+    scanf("%d", &b);
+    prod = a * b;
+    printf("Area Of Rectangle is = %d\n", prod);
+
+    return 0;
+}
