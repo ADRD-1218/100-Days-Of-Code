92f488ee9fe941a93e28cc03698a3d3288e29aa9
ADRD-1218 <aadirakshit1218@gmail.com>
2025-11-10 11:57:00 +0000

    Day 4

---
+++ Day_4_Q-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    float a;
+
+    printf("Value Of n = ");
+    scanf("%d", &n);
+    a = n * (n + 1) / 2;
+    printf("Sum Of First %d Numbers = %.2f\n ", n, a);
+
+    return 0;
+}
